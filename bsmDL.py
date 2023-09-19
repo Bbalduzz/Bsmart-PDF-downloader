f3eb62be78fb874155152b60cc7a1b591ac563c5
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2023-09-19 21:57:24 +0200

    Update bsmDL.py
    complete rework. 
    - Better tui
    . credentials insted of cookies (more intuitive)

--- bsmDL.py
+++ bsmDL.py
@@ -1,95 +1,107 @@
-import requests
-import msgpack
-import tarfile
+from Crypto.Util.Padding import unpad
+from Crypto.Cipher import AES
 from io import BytesIO
-from Cryptodome.Cipher import AES
-from Cryptodome.Util.Padding import unpad
+import requests
+import umsgpack
+import inquirer
+import tarfile
 import fitz
-import re
-import lib
 
-'''
-This script is just an over-simplification (or so, most of the parts are taken from FelixFrog tool)
-of @FelixFrog pdfgrabber tool. If u want something more complete and better made, check out his work 
-https://github.com/FelixFrog/pdfgrabber
-'''
+CREDS = ("your_mail_or_username", "your_password")
 
-key =  bytes([30, 0, 184, 152, 115, 19, 157, 33, 4, 237, 80, 26, 139, 248, 104, 155]) #'HgC4mHMTnSEE7VAai/homw=='
+class BSmartAPI:
+    BASE_URL = "https://www.bsmart.it/api/v5"
+    KEY = b'\x1e\x00\xb8\x98s\x13\x9d!\x04\xedP\x1a\x8b\xf8h\x9b'
 
-class URLS:
-	user: str = 'https://www.bsmart.it/api/v5/user'
-	lib: str = 'https://www.bsmart.it/api/v6/books?page_thumb_size=medium&per_page=25000'
-	book: str = 'https://www.bsmart.it/api/v6/books/by_book_id/'
-	books: str = 'https://api.bsmart.it/api/v5/books/'
+    def __init__(self):
+        self.session = requests.Session()
+        self.token = ''
 
-url = input('Enter the book url:\n')
-book_id = url.split('?')[0].removeprefix('https://my.bsmart.it/#/books/')
-revision = url.split('?')[1].removeprefix('revision=')
+    def _api_call(self, endpoint, method='GET', params=None, data=None, headers=None):
+        url = f"{self.BASE_URL}/{endpoint}"
+        response = self.session.request(method, url, params=params, data=data, headers=headers)
+        response.raise_for_status()
+        return response.json()
 
-def progress_bar(progress, total):
-    percent = 100 * (progress / float(total))
-    bar = '█' * int(percent) + '-' * (100 - int(percent))
-    print(f'\r[+] Downloading pack... |{bar}| {percent:.2f}%', end='\r')
+    def get_login_data(self, username, password):
+        return self._api_call("session", method='POST', data={"password": password, "email": username})
 
-def decryptfile(file): # ez
-	print('\r[+] Decrypting file...',end='\r')
-	header = msgpack.unpackb(file.read(256).rstrip(b"\x00"))
-	iv = file.read(16)
-	obj = AES.new(key, AES.MODE_CBC, iv)
-	dec = obj.decrypt(file.read(header[b"start"] - 256 - 16))
-	return unpad(dec, AES.block_size) + file.read(), header[b"md5"].decode()
+    def get_library(self):
+        return self._api_call("books", headers={"auth_token": self.token}, params={"per_page": 1000000, "page_thumb_size": "medium"})
 
-def downloadpack(url):
-	r = requests.get(url, stream=True)
-	length = int(r.headers.get("content-length", 1))
-	file = b""
-	n=0
-	progress_bar(n/100, 100)
-	for data in r.iter_content(chunk_size=102400):
-		file += data
-		n=n+1
-		progress_bar(n/100, 100)
-	return tarfile.open(fileobj=BytesIO(file))
+    def get_preactivations(self):
+        return self._api_call("books/preactivations", headers={"auth_token": self.token})
 
-with open('cookies.txt', 'r') as f:
-	cookie = f.read()
-	_bsw_session_v1_production = cookie.split(';')[0]
+    def get_book_info(self, bookid, revision, operation):
+        return self._api_call(f"books/{bookid}/{revision}/{operation}", headers={"auth_token": self.token}, params={"per_page": 1000000})
 
-auth_token = requests.get(URLS.user, headers={'cookie':_bsw_session_v1_production}).json()['auth_token']
-HEADERS = {'auth_token':auth_token}
+    def download_pack(self, url):
+        r = self.session.get(url, stream=True)
+        file = b""
+        for data in r.iter_content(chunk_size=204800):
+            file += data
+        return tarfile.open(fileobj=BytesIO(file))
 
-#library = requests.get(URLS.lib, headers=HEADERS)
-packs = requests.get(f'{URLS.books}/{book_id}/{revision}/asset_packs?per_page=1000000', headers=HEADERS).json() # --> page_pdf -> url
-resources = requests.get(f"{URLS.books}/{book_id}/{revision}/resources?per_page=500", headers=HEADERS).json()
-index = requests.get(f"{URLS.books}/{book_id}/{revision}/index", headers=HEADERS).json()
+    def decrypt_file(self, file):
+        header = umsgpack.unpackb(file.read(256).rstrip(b"\x00"))
+        iv = file.read(16)
+        obj = AES.new(self.KEY, AES.MODE_CBC, iv)
+        dec = obj.decrypt(file.read(header["start"] - 256 - 16))
+        return unpad(dec, AES.block_size) + file.read(), header["md5"]
 
-resmd5 = {}
-for i in resources:
-	if i["resource_type_id"] != 14:
-		continue
-	if pdf := next((j for j in i["assets"] if j["use"] == "page_pdf"), False):
-		resmd5[pdf["md5"]] = i["id"], i["title"]
+    def login(self, username, password):
+        login_data = self.get_login_data(username, password)
+        if "auth_token" not in login_data:
+            raise ValueError("There was and error while authentication: " + login_data["message"])
+        self.token = login_data["auth_token"]
 
-pagespdf, labelsmap = {}, {}
-pagespack = downloadpack(next(i["url"] for i in packs if i["label"] == "page_pdf"))
-for member in pagespack.getmembers():
-	file = pagespack.extractfile(member)
-	if file:
-		output, md5 = decryptfile(file)
-		pid, label = resmd5[md5]
-		pagespdf[pid] = output
-		labelsmap[pid] = label
-	pdf = fitz.Document()
-	toc, labels = [], []
+    def check_token(self, token):
+        test = self.get_library(token)
+        return "message" not in test
 
-bookmarks = {i["first_page"]["id"]:i["title"] for i in index}
-for i, (pageid, pagepdfraw) in enumerate(sorted(pagespdf.items())):
-	pagepdf = fitz.Document(stream=pagepdfraw, filetype="pdf")
-	pdf.insert_pdf(pagepdf)
-	labels.append(labelsmap[pageid])
-	if pageid in bookmarks:
-		toc.append([1, bookmarks[pageid], i + 1])
-pdf.set_page_labels(lib.generatelabelsrule(labels))
-pdf.set_toc(toc)
+    def library(self):
+        books = {str(book["id"]): {"title": book["title"], "revision": book["current_edition"]["revision"], "cover": book["cover"]} for book in self.get_library() if not book["liquid_text"]}
+        return books
 
-pdf.save(f'{book_id}.pdf')
+    def download_book(self, bookid, data):
+        revision = data["revision"]
+        resources = self.get_book_info(bookid, revision, "resources")
+        resmd5 = {}
+        for resource in resources:
+            if resource["resource_type_id"] != 14:
+                continue
+            if pdf := next((asset for asset in resource["assets"] if asset["use"] == "page_pdf"), False):
+                resmd5[pdf["md5"]] = resource["id"], resource["title"]
+
+        pagespdf = {}
+        asset_packs = self.get_book_info(bookid, revision, "asset_packs")
+        pagespack = self.download_pack(next(pack["url"] for pack in asset_packs if pack["label"] == "page_pdf"))
+        for member in pagespack.getmembers():
+            if file := pagespack.extractfile(member):
+                output, md5 = self.decrypt_file(file)
+                pid, label = resmd5[md5]
+                pagespdf[pid] = output
+
+        pdf = fitz.Document()
+        toc = []
+        index = self.get_book_info(bookid, revision, "index")
+
+        bookmarks = {entry["first_page"]["id"]: entry["title"] for entry in index if "first_page" in entry}
+        for i, (pageid, pagepdfraw) in enumerate(sorted(pagespdf.items())):
+            pagepdf = fitz.Document(stream=pagepdfraw, filetype="pdf")
+            pdf.insert_pdf(pagepdf)
+            if pageid in bookmarks:
+                toc.append([1, bookmarks[pageid], i + 1])
+
+        pdf.set_toc(toc)
+        pdf.save(f"{data['title']}.pdf")
+
+if __name__ == "__main__":
+    bsmart = BSmartAPI()
+    token = bsmart.login(*CREDS)
+    books = bsmart.library()
+
+    choice = inquirer.prompt([inquirer.List("book",message="Which book you want to download?",choices=[f"{book_data['title']}" for book_id, book_data in books.items()])])
+    chosen_book_id = next((book_id for book_id, book_data in books.items() if book_data['title'] == choice["book"]), None)
+    book_data = books[chosen_book_id] if chosen_book_id else "Book not found!"
+    bsmart.download_book(chosen_book_id, book_data)
